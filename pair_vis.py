a3c62662e2e01f8d2bc69e6262d42612fae4303d
kemfic1 <19ficici.kemal@gmail.com>
2018-11-29 13:12:31 -0500

    added point culling

--- pair_vis.py
+++ pair_vis.py
@@ -18,15 +18,18 @@ def main():
   disp_cam.SetHandler(handler)
   w = 250
   h = 50
-  img = cv2.imread('img/2_.jpg')#[:w,70:500]
+  img = cv2.imread('img/2.png')#[:w,70:500]
   disp = cv2.imread('img/out1.png',0) * 5.0#[:w,70:500] * 5.0
-
+  print(disp[0,200])
   # Add point locations using pixel location and z-distance from disparity map
   # lazy man's point cloud
-
+  mask = np.ones_like(disp) * 1.0
+  #mask[disp < 5.0] = 0.0
+  cv2.imshow('mask', mask*1000.0)
   grid = np.mgrid[0:img.shape[1], 0:img.shape[0]]
   grid = np.swapaxes(grid, 0,2)
   pts = np.dstack((grid,disp)) * 1.0
+  pts[pts[:,:,-1] < 100.0] = [0,0,0]
   pts = -1.0 * pts.reshape(pts.shape[0]*pts.shape[1], 3)
   # Color matrix based on point location
 
